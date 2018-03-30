e2cd89f5dddf1c708f809f8c1cb01120bbfbce60
takashi7 <konno.tlab@gmail.com>
2018-03-30 21:43:25 +0900

    add some functions

--- common/functions.py
+++ common/functions.py
@@ -22,3 +22,17 @@ def softmax(x):
     x = x - np.max(x)
     return np.exp(x) / np.sum(np.exp(x))
 
+def mean_squared_error(y, t):
+    return 0.5 * np.sum((y-t)**2)
+
+def cross_entropy_error(y, t):
+    if y.dim == 1:
+        t = t.reshape(1, t.size)
+        y = y.reshape(1, y.size)
+
+    if t.size == y.size:
+        t = t.argmax(axis=1)
+
+    batch_size = y.shape[0]
+    delta = 1e-7
+    return (-1) * np.sum(np.log(y[np.arange(batch_size), t] + delta)) / batch_size
