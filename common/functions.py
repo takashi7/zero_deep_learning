3a52a93511df14770803b4aa89270452331c72c8
takashi7 <konno.tlab@gmail.com>
2018-03-30 14:15:55 +0900

    add ch3

---
+++ common/functions.py
@@ -0,0 +1,24 @@
+import numpy as np 
+
+def identity_function(x):
+    return x
+
+def step_function(x):
+    return np.array(x>0, dtype=np.int)
+
+def sigmoid(x):
+    return 1/(1+np.exp(-x))
+
+def relu(x):
+    return np.maximum(0, x)
+
+def softmax(x):
+    if x.ndim == 2:
+        x = x.T 
+        x = x - np.max(x, axis=0)
+        y = np.exp(x) / np.sum(np.exp(x), axis=0)
+        return y.T
+    
+    x = x - np.max(x)
+    return np.exp(x) / np.sum(np.exp(x))
+
