0cea85d2176855442f6c92627fc24014732d5d7a
takashi7 <konno.tlab@gmail.com>
2018-03-31 17:29:56 +0900

    add ch4

--- common/functions.py
+++ common/functions.py
@@ -26,7 +26,7 @@ def mean_squared_error(y, t):
     return 0.5 * np.sum((y-t)**2)
 
 def cross_entropy_error(y, t):
-    if y.dim == 1:
+    if y.ndim == 1:
         t = t.reshape(1, t.size)
         y = y.reshape(1, y.size)
 
