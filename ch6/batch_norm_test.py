1f398a7f875f63637b41c3d37503d6760cf67c04
takashi7 <konno.tlab@gmail.com>
2018-04-02 21:04:29 +0900

    add ch6

--- ch6/batch_norm_test.py
+++ ch6/batch_norm_test.py
@@ -3,52 +3,82 @@ sys.path.append(os.pardir)
 import numpy as np 
 import matplotlib.pyplot as plt 
 from dataset.mnist import load_mnist
-from common.multi_layer_net import MultiLayerNet
-from common.optimizer import SGD
+from common.multi_layer_net_extend import MultiLayerNetExtend
+from common.optimizer import SGD, Adam
 
 # 0 Load MNIST Data
 (x_train, t_train), (x_test, t_test) = load_mnist(normalize=True)
+
+# 
+x_train = x_train[:1000]
+t_train = t_train[:1000]
+
+max_epochs = 20
 train_size = x_train.shape[0]
-batch_size = 128
-max_iteration = 2000
+batch_size = 100
+learning_rate = 0.01
 
-# 1 Initialize
-weight_init_types = {'std=0.01': 0.01, 'Xavier': 'sigmoid', 'He': 'relu'}
-optimizer = SGD(lr=0.01)
+# 
+def __train(weight_init_std):
+    bn_network = MultiLayerNetExtend(input_size=784, hidden_size_list=[100,100,100,100, 100], 
+                                     output_size=10, weight_init_std=weight_init_std, use_batchnorm=True)
+    network = MultiLayerNetExtend(input_size=784, hidden_size_list=[100,100,100,100, 100], 
+                                     output_size=10, weight_init_std=weight_init_std)
+    optimizer = SGD(lr=learning_rate)
 
-networks = {}
-train_loss = {}
-for key, weight_type in weight_init_types.items():
-    networks[key] = MultiLayerNet(input_size=784, hidden_size_list=[100,100,100,100], 
-                                  output_size=10, weight_init_std=weight_type)
-    train_loss[key] = []
+    train_acc_list = []
+    bn_train_acc_list = []
 
-# 2 Training
-for i in range(max_iteration):
-    batch_mask = np.random.choice(train_size, batch_size)
-    x_batch = x_train[batch_mask]
-    t_batch = t_train[batch_mask]
+    iter_per_epoch =  max(train_size / batch_size, 1)
+    epoch_cnt = 0
 
-    for key in weight_init_types.keys():
-        grads = networks[key].gradient(x_batch, t_batch)
-        optimizer.update(networks[key].params, grads)
-        loss = networks[key].loss(x_batch, t_batch)
-        train_loss[key].append(loss)
+    for i in range(1000000000):
+        batch_mask = np.random.choice(train_size, batch_size)
+        x_batch = x_train[batch_mask]
+        t_batch = t_train[batch_mask]
 
-    if i % 100 == 0:
-        print("-----" + "iteration: " + str(i) + "-----")
-        for key in weight_init_types.keys():
-            loss = networks[key].loss(x_batch, t_batch)
-            print(key + ":" + str(loss))
+        for _network in (bn_network, network):
+            grads = _network.gradient(x_batch, t_batch)
+            optimizer.update(_network.params, grads)
+
+        if i % iter_per_epoch == 0:
+            train_acc = network.accuracy(x_train, t_train)
+            bn_train_acc = bn_network.accuracy(x_train, t_train)
+            train_acc_list.append(train_acc)
+            bn_train_acc_list.append(bn_train_acc)
+            print("epoch:" + str(epoch_cnt) + " | " + str(train_acc) + " - " + str(bn_train_acc))
+            epoch_cnt += 1
+            if epoch_cnt >= max_epochs:
+                break
+    return train_acc_list, bn_train_acc_list
 
 
 # 3 plot
-markers = {'std=0.01': 'o', 'Xavier': 's', 'He': 'D'}
-x = np.arange(max_iteration)
-for key in weight_init_types.keys():
-    plt.plot(x, smooth_curve(train_loss[key]), marker=markers[key], markevery=100, label=key)
-plt.xlabel("iterations")
-plt.ylabel("loss")
-plt.ylim(0, 2.5)
-plt.legend()
+weight_scale_list = np.logspace(0, -4, num=16)
+x = np.arange(max_epochs)
+
+for i, w in enumerate(weight_scale_list):
+    print( "============== " + str(i+1) + "/16" + " ==============")
+    train_acc_list, bn_train_acc_list = __train(w)
+    
+    plt.subplot(4,4,i+1)
+    plt.title("W:" + str(w))
+    if i == 15:
+        plt.plot(x, bn_train_acc_list, label='Batch Normalization', markevery=2)
+        plt.plot(x, train_acc_list, linestyle = "--", label='Normal(without BatchNorm)', markevery=2)
+    else:
+        plt.plot(x, bn_train_acc_list, markevery=2)
+        plt.plot(x, train_acc_list, linestyle="--", markevery=2)
+
+    plt.ylim(0, 1.0)
+    if i % 4:
+        plt.yticks([])
+    else:
+        plt.ylabel("accuracy")
+    if i < 12:
+        plt.xticks([])
+    else:
+        plt.xlabel("epochs")
+    plt.legend(loc='lower right')
+    
 plt.show()
